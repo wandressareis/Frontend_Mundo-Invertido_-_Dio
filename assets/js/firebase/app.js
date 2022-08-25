e6b7a398f32aa6e09828667751dfc17fd26c3e37
Wandressa Reis <reiswandressa@gmail.com>
2022-08-25 18:02:37 -0400

    Banco de dados integrado

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDx-f7lt5Lzpc5tmaZyx7mm7atAwuZIgdE",
+    authDomain: "frontend-mundo-invertido-e61bc.firebaseapp.com",
+    projectId: "frontend-mundo-invertido-e61bc",
+    storageBucket: "frontend-mundo-invertido-e61bc.appspot.com",
+    messagingSenderId: "47859295906",
+    appId: "1:47859295906:web:4c6a8d6d78e04cfdfd59fb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app 
